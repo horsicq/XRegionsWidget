cf793136d3e33e4c5d585356fe1d9b0811668240
horsicq <horsicq@gmail.com>
2025-03-20 07:38:49 +0100

    Add new file(s): 2025-03-20

---
+++ xregionsmodel.cmake
@@ -0,0 +1,9 @@
+include_directories(${CMAKE_CURRENT_LIST_DIR})
+
+set(XREGIONSMODEL_SOURCES
+    ${XREGIONSMODEL_SOURCES}
+    ${CMAKE_CURRENT_LIST_DIR}/xregionitem.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xregionitem.h
+    ${CMAKE_CURRENT_LIST_DIR}/xregionsmodel.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xregionsmodel.h
+)
